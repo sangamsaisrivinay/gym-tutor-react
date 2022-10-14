ddd5081430c32769faa867c75e67fc636ed3b946
sangamsaisrivinay <saisrivinayreddysangam@gmail.com>
2022-10-14 06:06:33 +0530

    added files

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,6 @@ export {default as Footer} from './Footer';
 export {default as HeroBanner} from './HeroBanner';
 export {default as Exercises} from './Exercises';
 export {default as SearchExercises} from './SearchExercises';
+export {default as Detail} from './Detail';
+export {default as ExerciseVideos} from './ExerciseVideos';
+export {default as SimilarExercises} from './SimilarExercises';
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-
+import React,{useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {exerciseOptions, fetchData, youtubeOptions} from '../utils/fetchData'
+import {Detail, ExerciseVideos, SimilarExercises} from '../components'
 const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail]=useState({})
+  const [exerciseVideos, setExerciseVideos]=useState([])
+  const id= useParams()
+  useEffect(()=>{
+    const fetchExercisesData= async()=>{
+      const exerciseDbUrl='https://exercisedb.p.rapidapi.com'
+      const youtubeSearchUrl='https://youtube-search-and-download.p.rapidapi.com'
+      
+      const exerciseDetailData= await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
+      console.log(exerciseDetailData)
+      setExerciseDetail(exerciseDetailData)
+
+      const exerciseVideoData= await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions)
+      setExerciseVideos(exerciseVideoData.contents)
+    }
+  })
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail ExerciseDetail={exerciseDetail}/>
+      {/*console.log('in exerciseDetail', exerciseDetail)*/}
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
+      <SimilarExercises/>
+    </Box>
   )
 }
 
